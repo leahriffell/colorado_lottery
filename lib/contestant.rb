4db8dd5b4c9d39ada0f043c54a6e1bbe420539a6
Leah Riffell <leah.riffell@gmail.com>
2020-07-30 17:24:14 -0600

    Add initial tests and methods for Contestant

---
+++ lib/contestant.rb
@@ -0,0 +1,15 @@
+class Contestant
+  attr_reader :age, :state_of_residence, :spending_money
+  
+  def initialize(info)
+    @first_name = info[:first_name]
+    @last_name = info[:last_name]
+    @age = info[:age]
+    @state_of_residence = info[:state_of_residence]
+    @spending_money = info[:spending_money]
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+end
